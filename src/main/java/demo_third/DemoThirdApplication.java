bd81703ac7b1e3bfa84adcb70143d0238789d23b
bharatanarase66@gmail.com <bharatanarse66@gmail.com>
2022-01-10 11:06:36 +0530

    third

---
+++ src/main/java/demo_third/DemoThirdApplication.java
@@ -0,0 +1,13 @@
+package demo_third;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoThirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo_third/DemoThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package demo_third;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoThirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
